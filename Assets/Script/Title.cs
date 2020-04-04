57f655efb27af406beaa4bb8f14478bef5cae3b2
DAEN <jajugoguma@gmail.com>
2020-04-05 02:48:02 +0900

    첫번째 커밋   회원가입 기능 임시로 추가     어차피 DB 한번 날릴꺼니까 테스트 해도 무방     DB랑 바로 안붙기도 하고 보안상으로 좀 그래서 중간에 php 넣었음. 이것도 DB서버에 같이 올라가 있음!

---
+++ Assets/Script/Title.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SignUpButtonClicked()
+    {
+        SceneManager.LoadScene("SignUp");
+    }
+}
