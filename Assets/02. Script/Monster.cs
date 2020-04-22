a19a30926343d16a8b85601056315d19c1633a1e
mk <alsry567@gmail.com>
2020-04-22 16:39:36 +0900

    기타 수정

--- Assets/02. Script/Monster.cs
+++ Assets/02. Script/Monster.cs
@@ -5,52 +5,58 @@ using UnityEngine;
 
 
 
-public class Monster : Character
+public class Monster : MonoBehaviour
 {
     public Transform player;
     public Transform RayCam;
 
-    int MoveSpeed = 4;
-    int MinDist = 2;
-    int MaxDist = 10;
+    int MoveSpeed = 3;
+    int MinDist = 1;
+    int MaxDist = 5;
 
     Vector2 moveVec = Vector2.zero;
 
     public Vector2[] dir = null;
 
+    void Start()
+    {
+
+
+    }
     // Update is called once per frame
-    protected override void Update()
+    void Update()
     {
         
         if (Vector3.Distance(transform.position, player.position) <= MaxDist)
-        {   
-            Vector3 vec = player.position - transform.position;
-            vec.Normalize();
-            moveVec = vec;
+            if(Vector3.Distance(transform.position,player.position) >= MinDist)
+            {   
+                Vector3 vec = player.position - transform.position;
+                vec.Normalize();
+                moveVec = vec;
             
-            float absx = System.Math.Abs(moveVec.x);
-            float absy = System.Math.Abs(moveVec.y);
+                float absx = System.Math.Abs(moveVec.x);
+                float absy = System.Math.Abs(moveVec.y);
             
-            if (absx > absy)
-            {
-                if (moveVec.x > 0)
-                    RayCam.transform.position = (Vector2)transform.position + dir[1];
-                else if (moveVec.x < 0)
-                    RayCam.transform.position = (Vector2)transform.position + dir[0];
+                if (absx > absy)
+                {
+                    if (moveVec.x > 0)
+                        RayCam.transform.position = (Vector2)transform.position + dir[1];
+                    else if (moveVec.x < 0)
+                        RayCam.transform.position = (Vector2)transform.position + dir[0];
 
-            }
-            else if (absy > absx)
-            {
-                if (moveVec.y > 0)
-                    RayCam.transform.position = (Vector2)transform.position + dir[2];
-                else if (moveVec.y < 0)
-                    RayCam.transform.position = (Vector2)transform.position + dir[3];
-            }
-            Ray(moveVec);
+                }
+                else if (absy > absx)
+                {
+                    if (moveVec.y > 0)
+                        RayCam.transform.position = (Vector2)transform.position + dir[2];
+                    else if (moveVec.y < 0)
+                        RayCam.transform.position = (Vector2)transform.position + dir[3];
+                }
+                Ray(moveVec);
             
             
-            Debug.Log("chasing");
-        }
+                Debug.Log("chasing");
+            }
     }
 
     void Ray(Vector3 vec)
